a6d3c21cddb0204ae0b6c5e82f48a3e40780f140
Valsusa <valsusa@gmail.com>
2016-12-14 08:37:42 +0100

    Explicit Added

--- Explicit/1d1v-matlab/pic-2specie/pic.m
+++ Explicit/1d1v-matlab/pic-2specie/pic.m
@@ -1,3 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Explicit Matlab implementing the basic PIC method used in the Master Course:
+% Introduction to Plasma Dynamics (B-KUL-G0P71B)
+% https://arxiv.org/abs/1602.06326
+% https://perswww.kuleuven.be/~u0052182/
+% First implementation, September, 2010
+% License:  GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007
+% Copyright: KU Leuven
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 clear all
 close all
 
